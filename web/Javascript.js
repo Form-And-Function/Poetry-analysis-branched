b920123e1d9f2ef55f06674288f17b63f80ce1b6
StudentSydney <sydv793@gmail.com>
2018-08-08 13:26:58 -0700

    improve website, add tooltips

--- web/Javascript.js
+++ web/Javascript.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
 	$('.tooltip').tooltipster();
 
-	words = [];
-
-	function submit(){
+    function submit(){
 	    var input = document.getElementById("inputBox");
 	    var txt = $("#inputBox").val();
 		$.ajax("rest/poem",{data: {text: txt}}).done(function(result){
@@ -26,30 +24,46 @@ $(document).ready(function() {
         });
 	}
 
-	function addDevice(device, words, poem){
-	    poem.lines.devices.
-    }
-
 	function addLine(line) {
-	    var output =$("#output");
-	    line = "<br>" + line;
-	    output.appendChild(line);
+	    output.prepend("<br>")
+        $("#output").append(output);
 	}
 
 	function processLine(line){
-	    var lineHTML = "";
-	    line.words.each(function () {
+	    var nodes = [];
+	    var node;
+	    line.words.each(function (index, word) {
             var currentDevices = this.devices.map(i => poem.deviceList.ExtantDevices.i);
             if(currentDevices.length <= 0) {
-                lineHTML += line[i].text;
+                node = word.text;
             } else {
                 curretDevices.each(function () {
-                    var tooltipTxt = 'text="Device: '+this.Name+
-                        '\nStrength: '+this.intensity+'"';
-                    lineHTML = lineHTML + "<br>" + '<span class = "devices tooltip"'+tooltipTxt+'>' + line[i].text + "</span>"
+                    var node = $('<span class = "device"></span>')
+                        .text(word.text).
+                        tooltipster(this);
                 });
             }
+            nodes.add(node);
         });
-	    return lineHTML;
+	    return nodes;
 	}
+
+
+
+	////////////////tooltips////////////////////////
+    $('.device').tooltipster({
+
+        functionInit: function(instance, helper){
+
+            var content = instance.content();
+
+            instance.content(people);
+        },
+        // this formats the content on the fly when it needs to be displayed but does not modify its value
+        functionFormat: function(instance, helper, content){
+            var displayedContent = 'Device: '+this.Name+
+                '\nStrength: '+this.intensity;
+            return displayedContent;
+        }
+    });
 });
